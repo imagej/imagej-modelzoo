c14174ebe42f193a7b33f435856a763e9aa72342
jdeschamps <6367888+jdeschamps@users.noreply.github.com>
2021-03-12 15:12:33 +0100

    Happy new year 2021
    It's probably going to be better than last year...

--- src/main/java/net/imagej/modelzoo/consumer/model/node/DefaultModelZooNode.java
+++ src/main/java/net/imagej/modelzoo/consumer/model/node/DefaultModelZooNode.java
@@ -2,7 +2,7 @@
  * #%L
  * This is the bioimage.io modelzoo library for ImageJ.
  * %%
- * Copyright (C) 2019 - 2020 Center for Systems Biology Dresden
+ * Copyright (C) 2019 - 2021 Center for Systems Biology Dresden
  * %%
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
